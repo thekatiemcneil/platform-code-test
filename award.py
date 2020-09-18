cb81a1fb2eeb260b23f34053202f09a74063f83c
thekatiemcneil <thekatiemcneil@gmail.com>
2020-09-18 11:45:15 -0500

    move all award logic into individual classes.

--- award.py
+++ award.py
@@ -6,47 +6,53 @@ class Award(object):
 
     def update_quality(self, awards):
             for award in awards:
-                if award.name != 'Blue First' and award.name != 'Blue Compare':
-                    if award.quality > 0:
-                        if award.name == 'Blue Star':
-                            # award quality should lose two points only if it can do so without becoming negative
-                            if award.quality > 1:
-                                award.quality -=2
-                            # otherwise, award quality should simply equal zero
-                            else:
-                                award.quality = 0
-                        elif award.name != 'Blue Distinction Plus':
-                            award.quality -= 1
-                else:
-                    if award.quality < 50:
-                        if award.name != 'Blue Star':
-                            award.quality += 1
-                            if award.name == 'Blue Compare':
-                                if award.expires_in < 11:
-                                    if award.quality < 50:
-                                        award.quality += 1
-                                if award.expires_in < 6:
-                                    if award.quality < 50:
-                                        award.quality += 1
-
                 if award.name != 'Blue Distinction Plus':
-                    award.expires_in -= 1
-
-                if award.expires_in < 0:
-                    if award.name == 'Blue Star':
-                        # award quality should lose two points only if it can do so without becoming negative
-                        if award.quality > 1:
-                            award.quality -=2
-                        # otherwise, award quality should simply equal zero
-                        else:
-                            award.quality = 0
-                    elif award.name != 'Blue First':
-                        if award.name != 'Blue Compare':
-                            if award.quality > 0:
-                                if award.name != 'Blue Distinction Plus':
-                                    award.quality -= 1
-                        else:
-                            award.quality = award.quality - award.quality
+                    if award.name == 'Blue First':
+                        self.update_bf(award)
+                    elif award.name == 'Blue Compare':
+                        self.update_bc(award)
+                    elif award.name == 'Blue Star':
+                        self.update_bs(award)
                     else:
-                        if award.quality < 50:
-                            award.quality += 1
+                        self.update_gen(award)
+                    self.update_expires(award)
+                    self.fix_overages(award)
+
+    def update_gen(self, award):
+        if award.expires_in > 0:
+            award.quality -= 1
+        else:
+            award.quality -= 2
+
+    # update Blue Compare award
+    def update_bc(self, award):
+        if award.expires_in > 0:
+            award.quality += 1
+        if award.expires_in <= 10:
+            award.quality += 1
+        if award.expires_in <= 5:
+            award.quality += 1
+        if award.expires_in <= 0:
+            award.quality = 0
+
+    # update Blue First award
+    def update_bf(self, award):
+        award.quality += 1
+        if award.expires_in <= 0:
+            award.quality += 1
+
+    # update Blue Star award
+    def update_bs(self, award):
+        if award.expires_in > 0:
+            award.quality -= 2
+        else:
+            award.quality -= 4
+
+    def update_expires(self, award):
+        award.expires_in -= 1
+
+    def fix_overages(self, award):
+        if award.quality > 50:
+            award.quality = 50
+        if award.quality < 0:
+            award.quality = 0
